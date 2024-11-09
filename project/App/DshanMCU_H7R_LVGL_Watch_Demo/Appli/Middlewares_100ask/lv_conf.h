83607fe1d66e527b5edfb107aacc5d2f4c9a2103
YobeZhou <smilezyb@163.com>
2024-11-09 17:44:04 +0800

    fix(DshanMCU_H7R_LVGL_Watch_Demo): disable LV_USE_FS_FATFS

--- project/App/DshanMCU_H7R_LVGL_Watch_Demo/Appli/Middlewares_100ask/lv_conf.h
+++ project/App/DshanMCU_H7R_LVGL_Watch_Demo/Appli/Middlewares_100ask/lv_conf.h
@@ -722,7 +722,7 @@
 #endif
 
 /*API for FATFS (needs to be added separately). Uses f_open, f_read, etc*/
-#define LV_USE_FS_FATFS 1
+#define LV_USE_FS_FATFS 0
 #if LV_USE_FS_FATFS
     #define LV_FS_FATFS_LETTER '0'     /*Set an upper cased letter on which the drive will accessible (e.g. 'A')*/
     #define LV_FS_FATFS_CACHE_SIZE 0    /*>0 to cache this number of bytes in lv_fs_read()*/
